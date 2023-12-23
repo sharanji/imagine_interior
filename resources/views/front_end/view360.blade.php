c3ca437557bd386be96cca6f98e8ff199da0965f
sharanji <sharanjipoun@gmail.com>
2023-12-23 16:49:53 +0530

    added another 360

--- resources/views/front_end/view360.blade.php
+++ resources/views/front_end/view360.blade.php
@@ -3,5 +3,6 @@
 @section('content')
 
 <iframe src="https://home.by.me/en/project/6013425/media/22251069/view360?noUi" style="width: -webkit-fill-available!important;height:100vh;" frameborder="0"></iframe>
+<iframe src="https://home.by.me/en/project/6026422/media/22278645/view360?noUi" style="width: -webkit-fill-available!important;height:100vh;" frameborder="0"></iframe>
 
 @endsection
